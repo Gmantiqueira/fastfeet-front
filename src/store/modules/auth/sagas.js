e53369912cd3a97f1a86b95a8664a1d4569630e0
Gabriel Antiqueira <gmantiqueira@outlook.com>
2020-02-26 02:51:08 -0300

    Use token as auth in requests

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -18,6 +18,8 @@ export function* signIn({ payload }) {
 
         const { token, user } = response.data;
 
+        api.defaults.headers.Authorization = `Bearer ${token}`;
+
         yield put(signInSuccess(token, user));
 
         history.push('/delivery');
@@ -31,7 +33,18 @@ export function signOut() {
     history.push('/');
 }
 
+export function setToken({ payload }) {
+    if (!payload) return;
+
+    const { token } = payload.auth;
+
+    if (token) {
+        api.defaults.headers.Authorization = `Bearer ${token}`;
+    }
+}
+
 export default all([
+    takeLatest('persist/REHYDRATE', setToken),
     takeLatest('@auth/SIGN_IN_REQUEST', signIn),
     takeLatest('@auth/SIGN_OUT', signOut),
 ]);
