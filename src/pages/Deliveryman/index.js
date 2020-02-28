b59572ab0f8da26f66bbe4cd002053a46cf412c6
Gabriel Antiqueira <gmantiqueira@outlook.com>
2020-02-28 01:17:49 -0300

    Deliveryman Grid layout finished

---
+++ src/pages/Deliveryman/index.js
@@ -0,0 +1,93 @@
+import React from 'react';
+
+import Grid from '@/components/Grid';
+import ContentHeader from '@/components/ContentHeader';
+
+import AddIcon from '@/assets/add.svg';
+import CreateIcon from '@/assets/create.svg';
+import DeleteIcon from '@/assets/delete.svg';
+import SearchIcon from '@/assets/search.svg';
+import ViewIcon from '@/assets/visibility.svg';
+
+import { Container } from './styles';
+
+export default function Deliveryman(props) {
+    const gridSettings = [
+        {
+            title: 'ID',
+            key: 'id',
+            widthProportion: 1.3,
+        },
+        {
+            title: 'Foto',
+            key: 'avatar',
+            widthProportion: 1,
+        },
+        {
+            title: 'Nome',
+            key: 'name',
+            widthProportion: 1,
+        },
+        {
+            title: 'Email',
+            key: 'email',
+            widthProportion: 1,
+        },
+        {
+            title: 'Ações',
+            key: 'actions',
+            widthProportion: 1.3,
+        },
+    ];
+
+    const data = [
+        {
+            id: 0,
+            avatar: 'temporário',
+            name: 'Ludwig van Beethoven',
+            email: 'beethoven@gmail.com',
+        },
+        {
+            id: 1,
+            avatar: 'temporário',
+            name: 'Gabriel Antiqueira',
+            email: 'gmantiqueira@gmail.com',
+        },
+    ];
+
+    const actions = [
+        {
+            text: 'Editar',
+            icon: CreateIcon,
+            action: (data = 'teste edição') => console.log(data),
+        },
+        {
+            text: 'Excluir',
+            icon: DeleteIcon,
+            action: (data = 'teste excluir') => console.log(data),
+        },
+    ];
+
+    function handleRegisterLink() {
+        props.history.push('/register/deliveryman');
+    }
+
+    return (
+        <Container>
+            <ContentHeader
+                title="Gerenciando entregadores"
+                placeholder="Buscar por entregadores"
+            >
+                <button
+                    onClick={handleRegisterLink}
+                    className="primary"
+                    type="button"
+                >
+                    <img src={AddIcon} alt="Ícone de adicionar" />
+                    Cadastrar
+                </button>
+            </ContentHeader>
+            <Grid settings={gridSettings} data={data} actions={actions} />
+        </Container>
+    );
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,7 @@ import SignIn from '../pages/SignIn';
 
 import Delivery from '../pages/Delivery';
 import Recipient from '../pages/Recipient';
+import Deliveryman from '../pages/Deliveryman';
 
 import RegisterRecipient from '../pages/RegisterRecipient';
 
@@ -13,8 +14,11 @@ export default function Routes() {
     return (
         <Switch>
             <Route path="/" exact component={SignIn} />
+
             <Route path="/delivery" component={Delivery} isPrivate />
             <Route path="/recipient" component={Recipient} isPrivate />
+            <Route path="/deliveryman" component={Deliveryman} isPrivate />
+
             <Route
                 path="/register/recipient"
                 exact
